6faa4889eda6f9871ba33578e43ad81de0c4ced2
Khuong Tran <xuankhuong1402@gmail.com>
2024-03-10 16:53:23 +0700

    chan vai l

--- src/views/MoviePlayer.vue
+++ src/views/MoviePlayer.vue
@@ -1,5 +1,14 @@
 <template>
-  {{ movieData.name }}
+  <div class="w-full h-81">
+    <video
+      :src="movies[0]?.filename"
+      loop
+      muted
+      controls
+      type="video/mp4"
+    ></video>
+    {{ movies[0]?.filename }}
+  </div>
 </template>
 
 <script>
@@ -10,13 +19,19 @@ export default {
   props: ["slug"],
   setup(props) {
     const movieData = ref([]);
-
+    const movies = ref([]);
     onMounted(() => {
       axios
         .get(`https://ophim1.com/phim/${props.slug}`)
         .then((res) => {
           movieData.value = res.data.episodes;
+          const serverData = res.data.episodes.map(
+            (episode) => episode.server_data
+          );
+
+          movies.value = serverData.flat();
           console.log(movieData.value);
+          console.log(movies.value);
         })
         .catch((err) => {
           console.log(err);
@@ -25,6 +40,7 @@ export default {
 
     return {
       movieData,
+      movies,
     };
   },
 };
